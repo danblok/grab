5fcc0b6922b75f2e2f76fcb2615e3d5624719769
vacatixn <danytokmakov@gmail.com>
2024-01-06 18:05:46 +0500

    fix: overhaul printer
    Fix patterns so the end of a pattern exactly matches the end index of a word
    (previously the end was bigger than the actuall end by 1). In this regard change
    ranges in print functions.
    
    Change the sorting comparator so it compares the ends of patterns if their starts
    are equal.
    
    Interface out print functions so they read from a io.Reader and write to io.Writer.
    Add wrapper functions to pass io.Reader and set io.Writer to os.Stdout.
    
    Extract hardcoded input name placeholder to constant.

--- internal/printer/printer.go
+++ internal/printer/printer.go
@@ -4,36 +4,54 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
 	"slices"
 
 	"github.com/danblok/grab/internal/algorithm"
 	"github.com/fatih/color"
 )
 
+const NAME_PLACEHOLDER = "-------- %s --------\n"
+
 // stores range of a pattern
 type pattern struct {
 	start, end int
 }
 
+// Wrapper function around FprintDefault to make it output to stdout
+func PrintDefault(r io.Reader, name string, patterns []string) {
+	FprintDefault(r, os.Stdout, name, patterns)
+}
+
+// Wrapper function around FprintQuite to make it output to stdout
+func PrintQuite(r io.Reader, patterns []string) {
+	FprintQuite(r, os.Stdout, patterns)
+}
+
+// Wrapper function around FprintMinimum to make it output to stdout
+func PrintNonHuman(r io.Reader, patterns []string) {
+	FprintNonHuman(r, os.Stdout, patterns)
+}
+
 // Prints name of the output, then prints lines where patterns were found with line indicators.
 // The found patterns are emphasized with red color.
-func PrintDefault(reader io.Reader, inputName string, patterns []string) {
-	fmt.Printf("-------- %s --------\n", inputName)
+func FprintDefault(r io.Reader, w io.Writer, inputName string, patterns []string) {
+	fmt.Fprintf(w, NAME_PLACEHOLDER, inputName)
 	lineCount := 1
-	scanner := bufio.NewScanner(reader)
+	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		line := scanner.Text()
 		ps := accumulatePatterns(line, patterns)
-		ps = mergePatternsRanges(ps)
+		ps = mergePatternRanges(ps)
 		if len(ps) > 0 {
 			for i := range ps {
 				if i == 0 {
-					fmt.Printf("%d: %s%v", lineCount, line[:ps[i].start], color.RedString(line[ps[i].start:ps[i].end]))
+					fmt.Fprintf(w, "%d: %s%v", lineCount, line[:ps[i].start], color.RedString(line[ps[i].start:ps[i].end+1]))
 				} else {
-					fmt.Printf("%s%v", line[ps[i-1].end:ps[i].start], color.RedString(line[ps[i].start:ps[i].end]))
+					fmt.Fprintf(w, "%s%v", line[ps[i-1].end+1:ps[i].start], color.RedString(line[ps[i].start:ps[i].end+1]))
 				}
 			}
-			fmt.Printf("%s\n", line[ps[len(ps)-1].end:])
+			fmt.Fprintf(w, "%s\n", line[ps[len(ps)-1].end+1:])
 		}
 		lineCount++
 	}
@@ -41,42 +59,42 @@ func PrintDefault(reader io.Reader, inputName string, patterns []string) {
 
 // Prints lines where the patterns were found without line indicators and input name.
 // The found patterns are emphasized with red color.
-func PrintQuite(reader io.Reader, patterns []string) {
-	scanner := bufio.NewScanner(reader)
+func FprintQuite(r io.Reader, w io.Writer, patterns []string) {
+	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		line := scanner.Text()
 		ps := accumulatePatterns(line, patterns)
-		ps = mergePatternsRanges(ps)
+		ps = mergePatternRanges(ps)
 		if len(ps) > 0 {
 			for i := range ps {
 				if i == 0 {
-					fmt.Printf("%s%v", line[:ps[i].start], color.RedString(line[ps[i].start:ps[i].end]))
+					fmt.Fprintf(w, "%s%v", line[:ps[i].start], color.RedString(line[ps[i].start:ps[i].end+1]))
 				} else {
-					fmt.Printf("%s%v", line[ps[i-1].end:ps[i].start], color.RedString(line[ps[i].start:ps[i].end]))
+					fmt.Fprintf(w, "%s%v", line[ps[i-1].end+1:ps[i].start], color.RedString(line[ps[i].start:ps[i].end+1]))
 				}
 			}
-			fmt.Printf("%s\n", line[ps[len(ps)-1].end:])
+			fmt.Fprintf(w, "%s\n", line[ps[len(ps)-1].end+1:])
 		}
 	}
 }
 
 // Prints lines in format: "<line_number> <pattern1_start_idx> <pattern1_end_idx> <pattern2_start_idx> <pattern2_end_idx>"
 // The found patterns are emphasized with red color.
-func PrintMinimum(reader io.Reader, patterns []string) {
+func FprintNonHuman(r io.Reader, w io.Writer, patterns []string) {
 	lineCount := 1
-	scanner := bufio.NewScanner(reader)
+	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		line := scanner.Text()
 		ps := accumulatePatterns(line, patterns)
 		if len(ps) > 0 {
 			for i := range ps {
 				if i == 0 {
-					fmt.Printf("%d %s", lineCount, color.RedString(fmt.Sprintf("%d %d", ps[i].start, ps[i].end)))
+					fmt.Fprintf(w, "%d %s", lineCount, color.RedString(fmt.Sprintf("%d %d", ps[i].start, ps[i].end)))
 				} else {
-					fmt.Printf(" %s", color.RedString(fmt.Sprintf("%d %d", ps[i].start, ps[i].end)))
+					fmt.Fprintf(w, " %s", color.RedString(fmt.Sprintf("%d %d", ps[i].start, ps[i].end)))
 				}
 			}
-			fmt.Println()
+			fmt.Fprintln(w)
 		}
 		lineCount++
 	}
@@ -91,7 +109,7 @@ func accumulatePatterns(line string, patterns []string) []pattern {
 		patternIdxs := algorithm.Search(line, p)
 		mappedIdxs := make([]pattern, len(patternIdxs))
 		for i := range patternIdxs {
-			mappedIdxs[i] = pattern{patternIdxs[i], patternIdxs[i] + len(p)}
+			mappedIdxs[i] = pattern{patternIdxs[i], patternIdxs[i] + len(p) - 1}
 		}
 
 		idxs = append(idxs, mappedIdxs...)
@@ -99,12 +117,10 @@ func accumulatePatterns(line string, patterns []string) []pattern {
 
 	// sort the indicies by start index for better colorization
 	slices.SortFunc(idxs, func(a, b pattern) int {
-		if a.start < b.start {
-			return -1
-		} else if a.start > b.start {
-			return 1
+		if a.start == b.start {
+			return a.end - b.end
 		} else {
-			return 0
+			return a.start - b.start
 		}
 	})
 
@@ -112,7 +128,7 @@ func accumulatePatterns(line string, patterns []string) []pattern {
 }
 
 // Merges the ranges of patterns.
-func mergePatternsRanges(patterns []pattern) []pattern {
+func mergePatternRanges(patterns []pattern) []pattern {
 	if len(patterns) == 0 {
 		return []pattern{}
 	}
